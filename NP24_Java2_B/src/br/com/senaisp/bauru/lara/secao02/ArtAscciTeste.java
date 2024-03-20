ef20af2c0dba6d7458e4e31ac5e9f2475f3c173d
Lara-Herrera-07 <laraherrera2015.lh@gmail.com>
2024-03-20 18:36:51 -0300

    Commit Inicial

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/lara/secao02/ArtAscciTeste.java
@@ -0,0 +1,18 @@
+package br.com.senaisp.bauru.lara.secao02;
+
+public class ArtAscciTeste {
+
+	public static void main(String[] args) {
+		System.out.println("|\\/\\/\\/|  \r\n"
+				+ " |      |  \r\n"
+				+ " |      |  \r\n"
+				+ " | (o)(o)  \r\n"
+				+ " C      _) \r\n"
+				+ "  | ,___|  \r\n"
+				+ "  |   /    \r\n"
+				+ " /____\\    \r\n"
+				+ "/      \\");
+
+	}
+
+}
